1563128f16ee44ebaf1ce0290d5cf1b29754f51b
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-11-10 18:28:24 +0200

    database

--- dashboard/Controllers/email/userTemp.php
+++ dashboard/Controllers/email/userTemp.php
@@ -64,7 +64,7 @@
 											<td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
 												<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:20px;text-align:left;color:#525252;">
 													Just click on the following link and you will navigate to <strong>Hadef Bills</strong> account .<br>
-													<a href="http://localhost/crm" style="text-decoration: none;"><strong>Join Hadef Bills</strong></a><br>
+													<a href="https://bills.hadefit.com" style="text-decoration: none;"><strong>Join Hadef Bills</strong></a><br>
 												</div>
 											</td>
 										</tr>
