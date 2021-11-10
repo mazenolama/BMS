8be9508fa8a72b8e4d05af0319ec26bbf5dff759
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-11-10 21:03:58 +0200

    dashboard

--- dashboard/Controllers/ClientController.php
+++ dashboard/Controllers/ClientController.php
@@ -3,9 +3,12 @@
     require_once("database/Database.php");
     $con = dbConnection();
     $errors = array();
+
+    $parts = explode('/', $_SERVER["SCRIPT_NAME"]);
+    $file = $parts[count($parts) - 1];
     
     /***************         Get All Clients               ***************/
-        if(isset($_GET['page']) && $_GET['page'] =='View-Clients' || $_SERVER['SCRIPT_NAME'] =='index.php'){
+        if(isset($_GET['page']) && $_GET['page'] =='View-Clients' ||  $file =='index.php'){
             $query = "SELECT * FROM `clients` WHERE 1 ORDER BY created_at DESC";
             $execute = mysqli_query($con, $query);
             $fetch_clients = array();
