8a9701c73533778199ec6e4c52372c7f01825698
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-11-21 12:45:51 +0200

    email

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -148,6 +148,7 @@ if(isset($_POST['signup'])){
             if($execute){
                 $msg = "Account Resest (OTP) code ";
                 email_reset_user($fetch_user['fname'],$fetch_user['lname'],$email,$msg,$otp);
+                exit();
             }else{
                 $_SESSION['error'] = "Something went wrong!";
             }
