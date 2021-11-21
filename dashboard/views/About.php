220a90efffc6cb410b203ea2c20262321ae245ba
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-11-21 16:57:09 +0200

    about

--- dashboard/views/About.php
+++ dashboard/views/About.php
@@ -20,7 +20,7 @@
 
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label class="about-text"># Authentication system based on email verification (OTP) "from scratch" to authenticate users.</label>
+                                    <label class="about-text"># Authentication System based on email verification (OTP) "from scratch" to authenticate users.</label>
                                 </div>                                 
                             </div>
                             <div class="form-group row">
@@ -30,19 +30,20 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label class="about-text"># Users management : Create, Update, Delete, Active, Deactivate. <br> "Note: Once a new user add to the system H.B. will automatically send an email to the user contains on his/her login credentials." </label>
+                                    <label class="about-text"># Users Management : Create, Update, Delete, Active, Deactivate. <br> "Note: Once a new user add to the system H.B. will automatically send an email to the user contains on his/her login credentials." </label>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label class="about-text"># Clients management: Create, Update, Delete, Active, Deactivate.</label>
+                                    <label class="about-text"># Clients Management: Create, Update, Delete, Active, Deactivate.</label>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label class="about-text"># Invoices management: Create, Send Invoice, Update, Delete.</label>
+                                    <label class="about-text"># Invoices Management: Create, Send Invoice, Update, Delete.<br> "Note: Once a new user add to the system H.B. will automatically send an email to the user contains on his/her Bill Details." </label>
                                 </div>
                             </div>
+                            
                             <div class="form-group row" style="text-align: center;">
                                 <div class="col-md-12">
                                     <label class="about-text">Start Developing Date: 21/10/2021</label>
